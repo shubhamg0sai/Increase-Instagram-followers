0a14dddc2be19a6ee391e086e4d4d7932ee81fa7
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-04-22 21:36:38 +0530

    Update S.py

--- S.py
+++ S.py
@@ -1,9 +1,8 @@
 #CYBER NAME BLACK-KILLER
-#GITHUB: https://github.com/ShuBhamg0sain
-#WHATAPP NO +919557777030
+#GITHUB: https://www.instagram.com/shubham_g0sain
 import os
 CorrectUsername = "g0sain"
-CorrectPassword = "sim"
+CorrectPassword = "follow"
 
 loop = 'true'
 while (loop == 'true'):
@@ -24,63 +23,68 @@ import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,ge
 ##### LOGO #####
 import os
 os.system('clear')
-os.system("figlet -f pagga '      Facebook      ' | lolcat")
-os.system("figlet -f pagga ' Brute-Force ' | lolcat")
-os.system("figlet -f pagga '      Attack      ' | lolcat")
+os.system("figlet -f pagga 'increase' | lolcat")
+os.system("figlet -f pagga 'instagram' | lolcat")
+os.system("figlet -f pagga 'followers' | lolcat")
 logo='''
 \033[1;96m•◈•───────────────•◈•\033[1;92mShuBhamg0sain\033[1;96m•◈•───────────────•◈•
-\033[1;47m                  \033[1;31mShuBhamg0sain                \033[1;0m
 \x1b[1;93m--------------------------------------------------------------
 \x1b[1;92m➣  NAME  : Shubhamg0sain
 \x1b[1;91m➣  CYBER NAME : BLACK-KILLER
-\x1b[1;93m➣  WHATSAPP NO     : +919557777030
-\x1b[1;95m➣  WARNING  : DON,T CALL ME ONLY TEXT
-\x1b[1;97m➣  NOTE     : USE FAST 4G SIM NET
 \x1b[1;93m--------------------------------------------------------------"""
                                 '''
 def menu():
         print logo
-        print "\033[1;92mCYBER_HACKER_GLAXY_R.H.P_1.286-Wellcome"
         print
-        print "\033[1;91mATTACK ON Indian Ids"
+        print "\033[1;91mserver list "
         print "\033[1;92m[1]  page server 1"
         print "\033[1;92m[2]  page server 2"
         print "\033[1;92m[3]  page server 3"
         print "\033[1;92m[4]  page server 4"
-        print "\033[1;92m[5]  page server 5"
-        print "\033[1;92m[6]  UPDATE SYSTEM"
-        print "\033[1;92m[0]  FOR EXIT"
+        print "\033[1;91m[5]  page server 5"
+        print "\033[1;92m[6]  page server 6"
+        print "\033[1;92m[7]  follow me"
+        print "\033[1;91m[8]  UPDATE SCRIPT"
+        print "\033[1;91m[0]  FOR EXIT"
         print 50*'-'
         action()
 
 def action():
-        bch = raw_input('\n  ENTER HERE ANY NUMBER ')
+        bch = raw_input('\n  ENTER SERVER NUMBER')
         if bch =='':
+                print "\033[1;91mFill in correctly"
                 os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
                 action()   
+
+        elif bch =="1":
+                os.system('xdg-open https://chat.whatsapp.com/JtCW38B01hjAGwlVHhyu5q')             
+                
+        elif bch =="2":
+                os.system('xdg-open https://www.instagram.com/shubham_g0sai')
+                              
+        elif bch =="3":
+                os.system('xdg-open https://www.instagram.com/shubham_g0sai')
+                
         elif bch =="4":
-                os.system('xdg-open https://chat.whatsapp.com/JtCW38B01hjAGwlVHhyu5q')
-                print "\033[1;91mrun allsim by python2 S.py"
-                
-        elif bch =="5":
                 os.system('xdg-open https://www.instagram.com/shubham_g0sai')
-                print "\033[1;91mrun allsim by python2 S.py"
-                
-        elif bch =="5":
-                os.system('xdg-open https://www.instagram.com/shubham_g0sai')
-                print "\033[1;91mrun allsim by python2 S.py"
 
         elif bch =="5":
                 os.system('xdg-open https://www.instagram.com/shubham_g0sai')
-                print "\033[1;91mrun allsim by python2 S.py"
-
+ 
+        elif bch =="6":
+                os.system('xdg-open https://www.instagram.com/shubham_g0sai')
+  
+        elif bch =="7":
+                os.system('xdg-open https://www.instagram.com/shubham_g0sai')
+          
+        elif bch =="8":
+                os.system('xdg-open https://www.instagram.com/shubham_g0sai')
+ 
         elif bch =='0':
                 exb()
         else:
                 print '[!] Fill in correctly'
-                action()
-
-        
+                action()       
         raw_input('\n[Press Enter To Go Back]')
 
 
